f4b364d93a042b1568565f66c2ef024403ebcf21
Ykeishirou <100176536+keiprogram@users.noreply.github.com>
2024-06-14 08:58:46 +0000

    sasa

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,9 @@ st.markdown(
     <style>
     .centered-title {
         text-align: center;
-        color:#27bece;
+        display: inline-block;
+  background: linear-gradient(90deg, #4158D0, #C850C0 30%, #FFCC70);
+  background: -webkit-linear-gradient(0deg, #4158D0, #C850C0 30%, #FFCC70);
     }
     </style>
     """,
