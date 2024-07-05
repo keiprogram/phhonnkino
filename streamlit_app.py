ed6cbe9255b78f452d69e2f2e5b060e7607952c1
Ykeishirou <100176536+keiprogram@users.noreply.github.com>
2024-07-05 08:42:38 +0000

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ st.markdown(
         background-image: url('https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZDJ1MmoyejJ3bzQwNTJ0bDdmaGJhNTNwNHJlNjg4aTF6M3MyMWhxOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/5xtDarwBWrq3CBqqs5G/giphy.gif');
         background-size: cover;
         background-position: center;
-        height: 80vh;
+        height: 60vh;
         display: flex;
         flex-direction: column;
         justify-content: center;
